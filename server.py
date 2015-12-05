cc914f80ac1bd055d38e1f9a1d05e030eff48ed5
Joel Klabo <joelklabo@gmail.com>
2015-12-05 15:03:50 -0800

    Adding back payment

--- server.py
+++ server.py
@@ -7,18 +7,18 @@ import urllib.request
 import json
 
 # Import from the 21 Bitcoin Develper Library
-#from two1.lib.wallet import Wallet
-#from two1.lib.bitserv.flask import Payment
+from two1.lib.wallet import Wallet
+from two1.lib.bitserv.flask import Payment
 
 # Configure the app
 app = Flask(__name__)
-#wallet = Wallet()
-#payment = Payment(app, wallet)
+wallet = Wallet()
+payment = Payment(app, wallet)
 
 API_URL = 'http://api.icndb.com/jokes/random'
 
 @app.route('/joke')
-#@payment.required(1000)
+@payment.required(1000)
 def joke():
   response = urllib.request.urlopen(API_URL).read()
   response = response.decode('utf-8')
